9049b059bce7514bdd4d06e81827d144a48e3f59
VolodymyrKuzmenko <kuzmenko.volodimir@gmail.com>
2017-06-05 22:19:41 +0300

    implement L calculation

--- src/main/java/lab4/Lab4.java
+++ src/main/java/lab4/Lab4.java
@@ -9,8 +9,52 @@ public class Lab4 {
 
         int P = Integer.parseInt(args[1]);
         MPI.Init(args);
-        int me = MPI.COMM_WORLD.Rank();
+        int rank = MPI.COMM_WORLD.Rank();
         int N = 12;
+        double[][] MA = new double[N][];
+
+        double[][] L;
+        if (MPI.COMM_WORLD.Rank() == 0) {
+            MA = new double[][]{
+                    {10, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                    {1, 10, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                    {1, 1, 10, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                    {1, 1, 1, 10, 1, 1, 1, 1, 1, 1, 1, 1},
+                    {1, 1, 1, 1, 10, 1, 1, 1, 1, 1, 1, 1},
+                    {1, 1, 1, 1, 1, 10, 1, 1, 1, 1, 1, 1},
+                    {1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1, 1},
+                    {1, 1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1},
+                    {1, 1, 1, 1, 1, 1, 1, 1, 10, 1, 1, 1},
+                    {1, 1, 1, 1, 1, 1, 1, 1, 1, 10, 1, 1},
+                    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 10, 1},
+                    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 10}};
+        }
+
+
+        int h = N / P;
+
+        double[][] MAh = new double[h][N];
+        double[][] Lh = new double[h][N];
+        double[] L_input = new double[N];
+
+
+        MPI.COMM_WORLD.Scatter(MA, 0, h, MPI.OBJECT, MAh, 0, h, MPI.OBJECT, 0);
+
+        for (int k = 0; k < N - 1; k++) {
+            int beg_index = rank * h - h;
+            int end_index = beg_index + h - 1;
+            int local_k = 0;
+            if(k>=beg_index && k<=end_index){
+                local_k = k - beg_index;
+                for(int i = k+1; i < MAh.length; i++){
+                    Lh[local_k ][ i] = MAh[local_k] [ i] / MAh[local_k ][ k];
+                    L_input[i] = Lh[local_k ][ i];
+
+                }
+            }else if (k > end_index) continue;
+
+        }
+
 
         MPI.Finalize();
     }
