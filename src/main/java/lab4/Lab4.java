93c903bdab3fa8e731e29ddb6818fdb96bdd301c
VolodymyrKuzmenko <kuzmenko.volodimir@gmail.com>
2017-06-08 17:03:55 +0300

    finish lab4

--- src/main/java/lab4/Lab4.java
+++ src/main/java/lab4/Lab4.java
@@ -13,38 +13,28 @@ public class Lab4 {
         int P = Integer.parseInt(args[1]);
         MPI.Init(args);
         int rank = MPI.COMM_WORLD.Rank();
-        int N = 12;
+        int  N = 8;;
         double[][] MA = new double[N][];
 
-        double[][] L;
+        double[][] L = new double[N][N];
         if (rank == 0) {
             MA = new double[][]{
-                    {10, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-                    {1, 10, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-                    {1, 1, 10, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-                    {1, 1, 1, 10, 1, 1, 1, 1, 1, 1, 1, 1},
-                    {1, 1, 1, 1, 10, 1, 1, 1, 1, 1, 1, 1},
-                    {1, 1, 1, 1, 1, 10, 1, 1, 1, 1, 1, 1},
-                    {1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1, 1},
-                    {1, 1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1},
-                    {1, 1, 1, 1, 1, 1, 1, 1, 10, 1, 1, 1},
-                    {1, 1, 1, 1, 1, 1, 1, 1, 1, 10, 1, 1},
-                    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 10, 1},
-                    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 10}};
-            System.out.println("data inputted");
-        }
+                    {10, 1, 1, 1, 1, 1, 1, 1},
+                    {1, 10, 1, 1, 1, 1, 1, 1},
+                    {1, 1, 10, 1, 1, 1, 1, 1},
+                    {1, 1, 1, 10, 1, 1, 1, 1},
+                    {1, 1, 1, 1, 10, 1, 1, 1},
+                    {1, 1, 1, 1, 1, 10, 1, 1},
+                    {1, 1, 1, 1, 1, 1, 10, 1},
+                    {1, 1, 1, 1, 1, 1, 1, 10}};
 
+       }
 
         int h = N / P;
 
         double[][] MAh = new double[h][N];
         double[][] Lh = new double[h][N];
         double[] L_input = new double[N];
-
-
-        for (double[] item : MAh) {
-
-        }
 
         MPI.COMM_WORLD.Scatter(MA, 0, h, MPI.OBJECT, MAh, 0, h, MPI.OBJECT, 0);
 
@@ -57,24 +47,32 @@ public class Lab4 {
                 for (int i = k + 1; i < MAh.length; i++) {
                     Lh[local_k][i] = MAh[local_k][i] / MAh[local_k][k];
                     L_input[i] = Lh[local_k][i];
-
                 }
             }
 
             int sender_Task = k / h + 1;
 
-            if (sender_Task< P){
+            if (sender_Task < P) {
                 MPI.COMM_WORLD.Bcast(L_input, 0, N, MPI.DOUBLE, sender_Task);
             }
 
-
-
-
-
-
+            for (int j = local_k; j < h; j++) {
+                for (int i = k + 1; i < MAh[j].length; i++) {
+                    MAh[j][i] = L_input[i] * MAh[j][k];
+                }
+            }
         }
 
+        MPI.COMM_WORLD.Gather(MAh, 0, h, MPI.OBJECT, MA, 0, h, MPI.OBJECT, 0);
+        MPI.COMM_WORLD.Gather(Lh, 0, h, MPI.OBJECT, L, 0, h, MPI.OBJECT, 0);
 
+        if (rank == 0) {
+            double det = 1;
+            for (int i = 0; i < N; i++) {
+                det = det * MA[i][i] * L[i][i];
+            }
+            System.out.println(det);
+        }
         MPI.Finalize();
     }
 }
